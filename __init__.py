f0e130ca69b42cd8c999723f0744b7f3237dcf85
Andy McEvoy <mcevoy.andy@gmail.com>
2020-10-29 00:25:40 -0600

    Add Load URDF panel

--- __init__.py
+++ __init__.py
@@ -14,31 +14,71 @@ bl_info = {
 
 import bpy
 
-class UrdfLoader(bpy.types.Operator):
-    """Load a URDF into Blender."""
-    bl_idname = "object.urdf_loader"
-    bl_label = "URDF"
-    bl_options = {'REGISTER'}
+from bpy.props import (PointerProperty,
+                       StringProperty)
+from bpy.types import (Operator,
+                       PropertyGroup)
+
+from . load_urdf import UrdfLoader
+
+class UrdfProperties(PropertyGroup):
+    urdf_package: StringProperty(
+        name = 'URDF package',
+        description = 'desc',
+        default = '',
+        maxlen = 1024,
+    )
+
+class AddUrdf(Operator):
+    bl_label = 'Add URDF'
+    bl_idname = 'urdf.printout'
 
     def execute(self, context):
-        print('Load URDF.')
+        scene = context.scene
+        urdf_tool = scene.urdf_tool
+
+        test = UrdfLoader()
+        print('execute AddUrdf')
 
         return {'FINISHED'}
 
-def menu_func(self, context):
-    self.layout.operator(UrdfLoader.bl_idname)
 
-addon_keymaps = []
+class OBJECT_PT_UrdfPanel(bpy.types.Panel):
+    """Load a URDF into Blender."""
+    bl_label = 'URDF label'
+    bl_category = 'Load URDF'
+    bl_space_type = 'VIEW_3D'
+    bl_region_type = 'UI'
+
+    def draw(self, context):
+        layout = self.layout
+        layout.label(text="layout label")
+        scene = context.scene
+        urdf_tool = scene.urdf_tool
+
+        layout.prop(urdf_tool, "urdf_package")
+        layout.operator("urdf.printout")
+        layout.separator()
+
+classes = (
+    UrdfProperties,
+    AddUrdf,
+    OBJECT_PT_UrdfPanel
+)
 
 def register():
-    bpy.utils.register_class(UrdfLoader)
-
-    # Add
-    bpy.types.VIEW3D_MT_add.append(menu_func)
+    for cls in classes:
+        bpy.utils.register_class(cls)
+    bpy.types.Scene.urdf_tool = PointerProperty(type=UrdfProperties)
+    # bpy.types.VIEW3D_MT_add.append(menu_func)
+    print('Finished registering URDF addon')
 
 def unregister():
-    bpy.utils.unregister_class(UrdfLoader)
-    bpy.types.VIEW3D_MT_add.remove(menu_func)
+    for cls in classes:
+        bpy.utils.unregister_class(cls)
+    del bpy.types.Scene.urdf_tool
+    # bpy.types.VIEW3D_MT_add.remove(menu_func)
+    print('Finished unregistering URDF addon')
 
 if __name__ == "__main__":
     register()
---
+++ load_urdf.py
@@ -0,0 +1,8 @@
+import bpy
+
+class UrdfLoader():
+
+    def __init__(self):
+        super().__init__()
+
+        print('UrdfLoader init')
